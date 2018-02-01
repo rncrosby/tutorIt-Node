adeccf420c1cd101dd0a21246d440824886f9508
Robert Crosby <robertcrosby@dhcp-169-233-140-49.cruznet.ucsc.edu>
2018-02-01 12:01:42 -0800

    a

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var express = require('express')
+var braintree = require("braintree");
 var app = express()
+
+var gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: "jccpnwq2vh48x8fd",
+  publicKey: "gw4hwmmvycs5s4kf",
+  privateKey: "b93b07e5ee813d9bb37cddf917343ebd"
+});
 
 app.get('/', function (req, res) {
   res.send('Hello Digital Ocean!')
