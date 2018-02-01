2010036b803bbcfe084620303781526e2d452f9f
Robert Crosby <robertcrosby@dhcp-169-233-140-49.cruznet.ucsc.edu>
2018-02-01 12:15:27 -0800

    hy

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Digital Ocean!')
+   res.send('Hello World');
 })
 
-app.listen(3000, function () {
-  console.log('Magic is happening on port 3000!')
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
 })
