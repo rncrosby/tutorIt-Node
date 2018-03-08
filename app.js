f2d47c3ab2e7f1712c27da136e9f9bc9d1bdcb9d
Robert Crosby <robertcrosby@Roberts-MacBook-Pro.local>
2018-03-07 18:09:22 -0800

    a

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ var gateway = braintree.connect({
   privateKey: "b93b07e5ee813d9bb37cddf917343ebd"
 });
 
+var transporter = nodemailer.createTransport({
+ service: 'gmail',
+ auth: {
+        user: 'tutorit.development@gmail.com',
+        pass: '1EstateDr'
+    }
+});
+
 app.get("/client_token", (req, res) => {
   gateway.clientToken.generate({}, function (err, response) {
     if (err) {
@@ -22,7 +30,6 @@ app.get("/client_token", (req, res) => {
 })
 
 app.post("/checkout", function (req, res) {
-  console.log("checkout")
   var nonceFromTheClient = req.query.nonce
   gateway.transaction.sale({
   amount: "10.00",
@@ -38,6 +45,20 @@ app.post("/checkout", function (req, res) {
 app.post("/verifyEmail", function (req, res) {
   var email = req.query.email
   var code = req.query.code
+  var emailText = '<p>Below you will find your TutorIt verification code. Use this code from within the app to activate your account and get started!</p><br><center><b>'+code+'</b><br>'
+  const mailOptions = {
+    from: 'tutorit.development@gmail.com', // sender address
+    to: email, // list of receivers
+    subject: 'Your TutorIt Verification Code', // Subject line
+    html: emailText// plain text body
+  };
+
+  transporter.sendMail(mailOptions, function (err, info) {
+     if(err)
+       console.log(err)
+     else
+       console.log(info);
+  });
   console.log("verify email"+email+"code"+code)
 })
 
