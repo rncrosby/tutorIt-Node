61a874860dc3b3acd4b46b13cb4b0d015f86c21b
Robert Crosby <robertcrosby@Roberts-MacBook-Pro.local>
2018-03-07 22:19:14 -0800

    a

--- app.js
+++ app.js
@@ -62,14 +62,11 @@ app.post("/verifyEmail", function (req, res) {
     if (error) {
         return console.log(error);
     }
-    console.log('Message sent: %s', info.messageId);
-    // Preview only available when sending through an Ethereal account
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    return console.log('email sent!')
 
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   });
-  console.log("verify email"+email+"code"+code)
 })
 
 app.get('/', (req, res) => {
